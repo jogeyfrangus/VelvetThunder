6f8d816527cf8466e3f1e52b0e9cfa58f2cb59af
Nedtheyak <kschmidt36@my.bcit.ca>
2019-10-10 14:49:05 -0700

    Italian commit

---
+++ Thingy.js
@@ -0,0 +1,210 @@
+var textToEncode = "";
+
+function encode(textToEncode) {
+    var encodedText = "";
+    textToEncode = textToEncode.toLowerCase();
+    var textLength = textToEncode.length;
+    for (var i = 0; i < textLength; i++) {
+        switch(textToEncode[i]) {
+            case "a":
+                encodedText += "DiLOp";
+                break;
+            case "b":
+                encodedText += "PvHaO";
+                break;
+            case "c":
+                encodedText += "AcAtp";
+                break;
+            case "d":
+                encodedText += "gKyFe";
+                break;
+            case "e":
+                encodedText += "XeRPc";
+                break;
+            case "f":
+                encodedText += "GGQHs";
+                break;
+            case "g":
+                encodedText += "xhiVJ";
+                break;
+            case "h":
+                encodedText += "IHrJq";
+                break;
+            case "i":
+                encodedText += "ZAwZD";
+                break;
+            case "j":
+                encodedText += "GuHHn";
+                break;
+            case "k":
+                encodedText += "nuweC";
+                break;
+            case "l":
+                encodedText += "YrRJt";
+                break;
+            case "m":
+                encodedText += "DCzzv";
+                break;
+            case "n":
+                encodedText += "vWKua";
+                break;
+            case "o":
+                encodedText += "UgUuP";
+                break;
+            case "p":
+                encodedText += "MGCEP";
+                break;
+            case "q":
+                encodedText += "GHHKZ";
+                break;
+            case "r":
+                encodedText += "SyOzP";
+                break;
+            case "s":
+                encodedText += "sfdFF";
+                break;
+            case "t":
+                encodedText += "jhYUV";
+                break;
+            case "u":
+                encodedText += "oQUPQ";
+                break;
+            case "v":
+                encodedText += "HbBNc";
+                break;
+            case "w":
+                encodedText += "tynRH";
+                break;
+            case "x":
+                encodedText += "XuqpT";
+                break;
+            case "y":
+                encodedText += "qhFAs";
+                break;
+            case "z":
+                encodedText += "OFbMB";
+                break;
+            case "0":
+                encodedText += "pPvru";
+                break;
+            case "1":
+                encodedText += "HjOPF";
+                break;
+            case "2":
+                encodedText += "tRjnO";
+                break;
+            case "3":
+                encodedText += "Eupjt";
+                break;
+            case "4":
+                encodedText += "vVMVO";
+                break;
+            case "5":
+                encodedText += "awgTK";
+                break;
+            case "6":
+                encodedText += "qFQEZ";
+                break;
+            case "7":
+                encodedText += "CvdDY";
+                break;
+            case "8":
+                encodedText += "TvBxt";
+                break;
+            case "9":
+                encodedText += "BzsCS";
+                break;
+            case "!":
+                encodedText += "WVpBc";
+                break;
+            case "?":
+                encodedText += "ovIAs";
+                break;
+            case ".":
+                encodedText += "jHkKj";
+                break;
+            case ",":
+                encodedText += "hIMjF";
+                break;
+            case "@":
+                encodedText += "MIRom";
+                break;
+            case "#":
+                encodedText += "whfEQ";
+                break;
+            case "$":
+                encodedText += "LKrRe";
+                break;
+            case "%":
+                encodedText += "ZpuEA";
+                break;
+            case "^":
+                encodedText += "dLxgV";
+                break;
+            case "&":
+                encodedText += "vBMsw";
+                break;
+            case "*":
+                encodedText += "IhkOe";
+                break;
+            case "(":
+                encodedText += "iJyMM";
+                break;
+            case ")":
+                encodedText += "PHVyx";
+                break;
+            case "-":
+                encodedText += "YeMIA";
+                break;
+            case "_":
+                encodedText += "bNHdk";
+                break;
+            case "+":
+                encodedText += "KFwvH";
+                break;
+            case "=":
+                encodedText += "IiALi";
+                break;
+            case "[":
+                encodedText += "zdXCy";
+                break;
+            case "]":
+                encodedText += "EOEFw";
+                break;
+            case "{":
+                encodedText += "WQVoy";
+                break;
+            case "}":
+                encodedText += "yUQqN";
+                break;
+            case "~":
+                encodedText += "IyFUm";
+                break;
+            case "`":
+                encodedText += "gGYKd";
+                break;
+            case "'":
+                encodedText += "qAzZA";
+                break;
+            case '"':
+                encodedText += "vCOCn";
+                break;
+            case "/":
+                encodedText += "QOtBK";
+                break;
+            case "|":
+                encodedText += "HPmYX";
+                break;
+            case "<":
+                encodedText += "gQkaQ";
+                break;
+            case ">":
+                encodedText += "tRSTM";
+                break;
+            case " ":
+                encodedText += "qSbsY";
+                break;
+        }
+    }
+    return encodedText;
+}
